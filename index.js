2fe46ae74943b239733f129877515ebf37989663
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-05-26 20:34:14 +0100

    Implemented the function

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-
-const isValid = () => {
-  throw new Error("Remove this statement and implement this function");
+const isValid = (str) => {
+  var arr = str.split("-").join("").split("");
+	if(arr.length != 10) return false;
+	if(arr[9] == "X") arr[9] = 10;
+	return arr.reduce((a, v, i) =>
+		a + v * (10 - i)
+  , 0) % 11 == 0;
 };
 
 
